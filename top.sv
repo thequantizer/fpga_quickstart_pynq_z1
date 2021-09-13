fa75bd0c9c6a875484848bde103585cda0a7790a
thequantizer <76808892+thequantizer@users.noreply.github.com>
2021-09-13 03:55:18 -0500

    Update top.sv

--- top.sv
+++ top.sv
@@ -21,33 +21,48 @@
 
 
 module top(
-    input wire sysclk, 
+    input wire sysclk,
     input wire btn,
-    input wire reset,
+    input wire reset_btn,
     output logic [3:0] led
-    );
-    
+);
+
     //initalize bit to 0
     (* mark_debug = "true" *) bit btn_pressed;
-    
+    bit reset;
+    initial reset = 1'b0;
+
     //debounce module will pulse every time a button is pressed
-    debounce #(.DEBOUNCE_COUNT_THRESHOLD(300)) debounce_mod(.*);
-    
+    debounce #(.DEBOUNCE_COUNT_THRESHOLD(300)) debounce_reset(
+        //input
+        .reset(1'b0),
+        .sysclk(sysclk),
+        .btn(reset_btn),
+        //output
+        .btn_pressed(reset));
+    debounce #(.DEBOUNCE_COUNT_THRESHOLD(300)) debounce_btn(
+        //input
+        .reset(reset),
+        .sysclk(sysclk),
+        .btn(btn),
+        //output
+        .btn_pressed(btn_pressed));
+
     logic [3:0] count = '0;
-        
+
     always @(posedge btn_pressed or posedge reset) begin
         if(reset) begin
-            count = '0;
-        end 
+            count <= '0;
+        end
         else begin
-        count = count + 1;
+            count <= count + 1;
         end
     end
-    
+
     always @(posedge sysclk or posedge reset) begin
         if(reset) begin
-            led = '0;
-        end 
+            led <= '0;
+        end
         else begin
             led[0] <= count[0];
             led[1] <= count[1];
